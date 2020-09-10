ebf99c6272b0da9a63e7f687e0bc9915f57be180
ravisaxena23 <saxenaravi23599@gmail.com>
2020-09-10 23:44:41 +0530

    form corrected

--- decentralised_voting/src/Home.js
+++ decentralised_voting/src/Home.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import {Row,Col,Input,Button,Label,Form,FormGroup,FormText} from 'reactstrap'
+import React, { Component } from 'react';
+import { Row, Col, Input, Button, Label, Form, FormGroup, FormText } from 'reactstrap'
 //import Web3 from 'web3';
-const abi=[
+const abi = [
   {
     "inputs": [],
     "payable": false,
@@ -66,52 +66,70 @@ console.log(contract);*/
 
 
 class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      contestantId:0,
-      contestantAddress:''
+    this.state = {
+      contestantId: "",
+      contestantAddress: ""
     }
-    this.handleSubmit=this.handleSubmit.bind(this);
-    this.handleChange=this.handleChange.bind(this);
+    // this.handleSubmit=this.handleSubmit.bind(this);
+    // this.handleChange=this.handleChange.bind(this);
   }
 
-  handleChange(event){
+  handleChange(e) {
     //console.log(event);
-    event.persist()
-    const name=event.target.name;
-    this.setState({[name]:event.target.value})
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   }
 
-  handleSubmit(event){
-    event.preventDefault();
-    console.log(event.target.value);
+  onSubmit = e => {
+    e.preventDefault();
+    if (!this.state.contestantId) {
+      console.log("No id")
+    }
+    else {
+      console.log(this.state.contestantId)
+    }
+    this.setState({
+      contestantId: "",
+      contestantAddress: ""
+    });
+
   }
 
-  render(){
+  render() {
     //console.log(web3.version);
-    return(
+    return (
       <div className="container">
         <h1> Welcome to decentralised Voting </h1>
-        <Form onSubmit={this.handleSubmit} >
-      <Row form>
-        <Col style={{margin:"70px 70px 0px 70px"}} md={{size:2,offset:2}}>
-          <FormGroup>
-            <Label for="contestantId">Contestant ID</Label>
-            <Input onChange={this.handleChange} value={this.state.contestantId} type="text" name="contestantId" id="contestantId" placeholder="contestantId" />
-          </FormGroup>
+        <form>
+          <Col style={{ margin: "70px 70px 0px 70px" }} md={{ size: 2, offset: 2 }}>
+            <FormGroup>
+              <Label for="contestantId">Contestant ID</Label>
+              <Input
+                type="text"
+                onChange={e => this.handleChange(e)}
+                value={this.state.contestantId} type="text"
+                name="contestantId"
+                placeholder="contestantId" />
+            </FormGroup>
+          </Col>
+          <Col style={{ margin: "70px 70px 0px 70px" }} md={{ size: 6, offset: 1 }}>
+            <FormGroup>
+              <Label for="contestantAddress">Contestant Address</Label>
+              <Input
+                type="text"
+                name="contestantAddress"
+                onChange={e => this.handleChange(e)}
+                value={this.state.contestantAddress}
+                id="contestantAddress" placeholder="contestantAddress" />
+            </FormGroup>
+          </Col>
+        </form>
+        <Col md={{ offset: 3 }}>
+          <Button type="submit" outline color="primary" onClick={(e) => this.onSubmit(e)}> Add contestant </Button>
         </Col>
-        <Col style={{margin:"70px 70px 0px 70px"}} md={{size:6,offset:1}}>
-          <FormGroup>
-            <Label for="contestantAddress">Contestant Address</Label>
-            <Input type="text" name="contestantAddress" onChange={this.handleChange} value={this.state.contestantAddress} id="contestantAddress" placeholder="contestantAddress" />
-          </FormGroup>
-        </Col>
-      </Row>
-      <Col  md={{offset:3}}>
-        <Button  type="submit"  outline color="primary"> Add contestant </Button>
-      </Col>
-      </Form>
       </div>
     )
   }
